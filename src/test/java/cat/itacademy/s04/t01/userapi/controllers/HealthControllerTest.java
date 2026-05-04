86b58794578a0699cfe4e650eab0eabe567fff93
erictc95 <maxerictc95@hotmail.com>
2026-05-04 14:02:28 +0200

    feat: return health check response as json

---
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
@@ -0,0 +1,10 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+
+public class HealthControllerTest {
+
+    //@WebMvcTest(HealthController.class)
+
+
+}
