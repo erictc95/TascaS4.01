b27b2387f9666c66e74d42cf11ed78999f783729
erictc95 <maxerictc95@hotmail.com>
2026-05-05 11:22:04 +0200

    feat: initial test completed. update: pom.

--- src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
@@ -1,10 +1,24 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthController.class)
 public class HealthControllerTest {
 
-    //@WebMvcTest(HealthController.class)
+    @Autowired
+    private MockMvc mockMvc;
 
-
+    @Test
+    void shouldReturnOkStatus() throws Exception {
+        mockMvc.perform(get("health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"));
+    }
 }
