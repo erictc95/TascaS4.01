7bb58bc68d9ebe6225c16f6b21e5d01a816a676a
erictc95 <maxerictc95@hotmail.com>
2026-06-02 17:30:35 +0200

    feat: repository.

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/repository/InMemoryUserRepository.java
@@ -0,0 +1,41 @@
+package cat.itacademy.s04.t01.userapi.repository;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository  {
+
+    private final List<User> users = new ArrayList<>();
+
+    @Override
+    public User save(User user) {
+        users.add(user);
+        return user;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return users;
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return users.stream().filter(user -> user.id().equals(id)).findFirst();
+    }
+
+    @Override
+    public List<User> searchByName(String name) {
+        return users.stream().filter(user -> user.name().toLowerCase().contains(name.toLowerCase())).toList();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return users.stream().anyMatch(user -> user.email().equals(email));
+    }
+}
--- src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
@@ -6,7 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,7 +19,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
     @Autowired
